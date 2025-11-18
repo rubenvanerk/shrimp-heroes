0ee0f17df813208cf907b86f8ae1f4d4b915d5cb
Ruben van Erk <ruben@rubenvanerk.nl>
2025-11-18 08:09:21 +0100

    update font

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -14,7 +14,7 @@ export default function Welcome({
             <Head title="Welcome">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
-                    href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
+                    href="https://fonts.bunny.net/css?family=outfit:400,500,600,700"
                     rel="stylesheet"
                 />
             </Head>
